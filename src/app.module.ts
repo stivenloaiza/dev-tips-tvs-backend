983df754a5cfcadd0bd016b639e3615e49947ff8
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-09 09:56:56 -0500

    User data was burned to test endpoint consumption [HU-TV-25].

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,24 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from './persistence/persistence.module';
 import { ConfigModule } from '@nestjs/config';
 import db_config from './persistence/db-config';
+import { HttpModule } from '@nestjs/axios';
+import { UserSuscriptionModule } from './user-suscription/user-suscription.module';
+import { UserSubscriptionsController } from './user-suscription/user-suscription.controller';
+import { UserSuscriptionService } from './user-suscription/user-suscription.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-      load:[db_config],
+      load: [db_config],
       isGlobal: true,
     }),
 
-    PersistenceModule
+    HttpModule,
+    PersistenceModule,
+    UserSuscriptionModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserSubscriptionsController],
+  providers: [UserSuscriptionService],
 })
 export class AppModule {}
