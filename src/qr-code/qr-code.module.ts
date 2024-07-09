4444d32872afbcb27143da9c44ba1369e3cb3c50
Samuel Sml <veramirandasamuel6@gmail.com>
2024-07-09 13:23:14 -0500

    feat: Add VerificationCode entity to QrCodeModule
    Included the VerificationCode schema in the QrCodeModule to store generated QR codes with fields for email, used status, code, creation date, and optional expiration date.

--- src/qr-code/qr-code.module.ts
+++ src/qr-code/qr-code.module.ts
@@ -3,10 +3,17 @@ import { QrCodeController } from './controllers/qr-code.controller';
 import { QrCodeService } from './services/qr-code.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { QrCode, QRCodeSchema } from './entities/qr-code.entity';
+import {
+  VerificationCode,
+  VerificationCodeSchema,
+} from './entities/verification-codes.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: QrCode.name, schema: QRCodeSchema }]),
+    MongooseModule.forFeature([
+      { name: QrCode.name, schema: QRCodeSchema },
+      { name: VerificationCode.name, schema: VerificationCodeSchema },
+    ]),
   ],
   controllers: [QrCodeController],
   providers: [QrCodeService],
