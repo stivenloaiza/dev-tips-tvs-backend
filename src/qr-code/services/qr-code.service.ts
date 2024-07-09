50c381a62206fd74c183953f7d87a381de5e2950
Samuel Sml <veramirandasamuel6@gmail.com>
2024-07-09 08:02:39 -0500

    feat: Add userExists method with hardcoded user data for email validation
    Added a new method userExists in QrCodeService to simulate email validation using hardcoded user data. This allows for temporary validation of user emails until the user microservice is available for integration.

--- src/qr-code/services/qr-code.service.ts
+++ src/qr-code/services/qr-code.service.ts
@@ -37,4 +37,33 @@ export class QrCodeService {
     const qrCode = await this.qrCodeModel.findOne({ code: code }).exec();
     return qrCode ? qrCode.isAuthenticated : false;
   }
+
+  async userExists(email: string): Promise<Boolean> {
+    // JSON burned to simulate email validation
+    const users = [
+      {
+        apiKey: 'xyz7890abcdef',
+        name: 'Bob Johnson',
+        email: 'bob@example.com',
+        phone: '1987654321',
+        role: 'company',
+        managerName: 'Jane Doe',
+        managerEmail: 'jane.doe@example.com',
+        managerPhone: '+1122334455',
+        subscriptions: 'TvMedia',
+      },
+      {
+        apiKey: 'xyz7890abcde2',
+        name: 'Steve Smith',
+        email: 'steve@example.com',
+        phone: '3524154863',
+        role: 'company',
+        managerName: 'Jane Doe',
+        managerEmail: 'jane.doe@example.com',
+        managerPhone: '+1122334455',
+        subscriptions: 'TvMedia',
+      },
+    ];
+    return users.some((user) => user.email === email);
+  }
 }
