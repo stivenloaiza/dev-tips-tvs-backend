51cb5e9a8c024dede6cf6e9ac39f3406f3251731
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-11 20:04:43 -0500

    Method name change and modification of initial business logic, creating a method that takes an API key to consume the user API, destructures it to obtain the subscription. Then, it consumes the tips API, which requires a level and language to return a random tip.

---
+++ src/modules/api-key/services/apikey.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { AxiosService } from 'src/modules/axios/axios.service';
+
+@Injectable()
+export class ApiKeyAuthService {
+  constructor(private readonly axiosService: AxiosService) { }
+
+  async startByApiKey(apiKey: string): Promise<any> {
+    try {
+      const user = await this.axiosService.get(
+        `http://localhost:3000/v1/api/subscriptions/sub?apiKey=${apiKey}`,
+      );
+
+      const {
+        name,
+        subscriptions: [{
+          technology: [{ name: technologyName }],
+          level: [{ name: levelName }]
+        }]
+      } = user;
+
+      const sub = { "Name": name, "technology": technologyName, "level": levelName };
+
+      const tip = await this.axiosService.get(`http://localhost:3000/v1/api/mock-tips/tips?level=${levelName}&technology=${technologyName}`,)
+      return { "tip": tip }
+    } catch (error) {
+      throw new NotFoundException(`Api key ${apiKey} not found`);
+    }
+  }
+}
