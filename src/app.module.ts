069b757ad49152da1619e58bfcae390aa8f70fdc
Samuel Sml <veramirandasamuel6@gmail.com>
2024-07-12 08:43:53 -0500

    refactor: move 'qr-code' folder to 'modules' directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PersistenceModule } from './modules/persistence/persistence.module';
 import { ConfigModule } from '@nestjs/config';
 import { ApikeyModule } from './modules/api-key/apikey.module';
-import { QrCodeModule } from './qr-code/qr-code.module';
+import { QrCodeModule } from './modules/qr-code/qr-code.module';
 import db_config from './modules/persistence/db-config';
 import { HttpModule } from '@nestjs/axios';
 import { MockTipsModule } from './mocks/mock-tips/mock-tips.module';
