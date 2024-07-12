b6ad613dbe8147ce9c9aed4021233c177dd0a083
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-11 20:13:40 -0500

    The folder locations have been modified.

---
+++ src/modules/axios/axios.service.ts
@@ -0,0 +1,13 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class AxiosService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async get(url: string, config?: any) {
+    const response = await firstValueFrom(this.httpService.get(url, config));
+    return response.data;
+  }
+}
