fb6d06457f875531718a320703678527d9705400
ElverTunubala <elvertunubala@gmail.com>
2024-07-12 11:11:11 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   const port = process.env.PORT || 4000;
 
   app.setGlobalPrefix('/v1/api');
