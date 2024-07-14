7c2b546037e00ab67774835b11cb101a591bf34b
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-14 17:56:39 -0500

    global pipe configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,16 @@ async function bootstrap() {
   const port = process.env.PORT || 4000;
 
   app.setGlobalPrefix('/v1/api');
+
+  app.useGlobalPipes(new ValidationPipe(
+    {
+       whitelist: true,
+      forbidNonWhitelisted: true,
+     transformOptions:{
+       enableImplicitConversion: true,
+     }
+   }
+  ));
 
   const config = new DocumentBuilder()
     .setTitle('TV´s Tips')
