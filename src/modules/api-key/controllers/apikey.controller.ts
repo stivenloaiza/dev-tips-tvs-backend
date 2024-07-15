613d383c1fdf6dbf2ab28d89413afe7ef2e1fa47
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-15 08:42:07 -0500

    Add Swagger documentation for ApiKeyAuthController

--- src/modules/api-key/controllers/apikey.controller.ts
+++ src/modules/api-key/controllers/apikey.controller.ts
@@ -1,10 +1,27 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiKeyAuthService } from '../services/apikey.service';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Validate')  
 @Controller('auth')
 export class ApiKeyAuthController {
   constructor(private readonly apiKeyAuthService: ApiKeyAuthService) { }
 
+
+  @ApiParam({
+    name:'apiKey',
+    type:'string',
+    required: true,
+    description: 'The apikey of the tip to be found.',
+    examples:{
+      example1:{
+        value:'xyz7890abcde2'
+      }
+    } 
+  })
+  @ApiOperation({ summary: 'Find the susbcription by apikey of the system.', description: 'View a specific tip registered in the database.' })
+  @ApiResponse({ status: 201, description: 'The record has been successfully created.'})
+  @ApiResponse({ status: 403, description: 'Forbidden.'})
   @Get('validate-apikey')
   async validateApiKey(@Query('apiKey') apiKey: string) {
     return this.apiKeyAuthService.startByApiKey(apiKey);
