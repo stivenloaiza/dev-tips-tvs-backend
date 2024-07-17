a2085d6ddfc3c137771fb6c1e9a5db5e56f20d05
Arangog20 <149792894+Arangog20@users.noreply.github.com>
2024-07-17 08:45:58 -0500

    code refinement to be tested with front, consuming user microservice and test mock tips

--- src/modules/api-key/services/apikey.service.ts
+++ src/modules/api-key/services/apikey.service.ts
@@ -4,31 +4,23 @@ import { AxiosService } from 'src/modules/axios/axios.service';
 
 @Injectable()
 export class ApiKeyAuthService {
-  constructor(private readonly axiosService: AxiosService) {}
+  constructor(private readonly axiosService: AxiosService) { }
 
   async startByApiKey(apikey: string): Promise<any> {
     try {
       const user = await this.axiosService.get(
-        //`http://localhost:3000/v1/api/subscriptions/sub?apiKey=${apikeyDto}`,
-         ` http://localhost:3001/v1/api/tvs/getApiKey/${apikey} `
+        ` http://localhost:3001/v1/api/tvs/getApiKey/${apikey} `
       );
-       const { 
-        level, 
-        technology, 
-        userId : {name}
-      } = user; 
-
-      /* const {
-        technology: [{ name: technologyName }],
-        levels: [{ name: levelName }],
-          
-      } = user; */
-
-      /* const tip = await this.axiosService.get(
-        `http://localhost:3000/v1/api/mock-tips/tips?level=${level}&technology=${technology}`,
-      ); */
-      return {level, technology , name}
-      //{ /*tip: tip,*/ user: name,};
+      const {
+        level,
+        technology,
+        userId: { name }
+      } = user;
+        const tip = await this.axiosService.get(
+        `http://localhost:3000/api/v1/mock-tips/tips?level=${level}&technology=${technology}`,
+      );  
+      return {tip: tip, name }
+      // { /*tip: tip,*/ user: name,};
     } catch (error) {
       throw new NotFoundException(`Api key ${apikey} not found`);
     }
