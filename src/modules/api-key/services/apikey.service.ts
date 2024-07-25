eb70a0bffc91b2e00ee23f647f330aefa5b307bc
daniejl04 <danielestebanjimenezlopez@gmail.com>
2024-07-25 12:46:14 -0500

    connect back-end y front-end

--- src/modules/api-key/services/apikey.service.ts
+++ src/modules/api-key/services/apikey.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import axios from 'axios';
 
-
 @Injectable()
 export class ApiKeyAuthService {
 
   async startByApiKey(apikey: string): Promise<any> {
     try {
-      const response = await axios.get(`http://localhost:3000/v1/api/tvs/getApiKey/${apikey}`);
+      const urlUser = process.env.USER_URL
+      const response = await axios.get(`${urlUser}/v1/api/tvs/getApiKey/${apikey}`);
+      
       const user = response.data;
       
       const {
@@ -15,19 +16,17 @@ export class ApiKeyAuthService {
         technology,
         userId: { name }
       } = user;
- 
-      const tipResponse = await axios.get(`http://localhost:3000/tips/all?`, {
-        params: {
-          level,
-          technology,
+      const urlTips = process.env.TIPS_URL
+
+      const tipResponse = await axios.get(`${urlTips}/tips/random?limit=1&level=${level}&technology=${technology}`, {
+        headers:{
+          "x-api-key": process.env.TVS_APIKEY
         }
-      });
-
+      }, );
+           
       const tip = tipResponse.data;
-      const randomIndex = Math.floor(Math.random() * tip.length);
-      const randomTip = tip[randomIndex];
-
-      return { randomTip, name };
+      
+      return { tip, name };
     } catch (error) {
       if (error.response && error.response.status === 404) {
         throw new NotFoundException(`API key ${apikey} not found`);
